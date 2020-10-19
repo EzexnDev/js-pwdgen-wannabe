9b782f5bac8c408c7673a9426827c85b6047eb28
Gianmarco Cavaliere <gianmarco.cavalieredev@gmail.com>
2020-10-19 15:18:58 +0200

    Aggiunta del 19

--- script/script.js
+++ script/script.js
@@ -4,4 +4,4 @@ var surname = prompt("Il tuo cognome?");
 
 var prefColor = prompt("Il tuo colore preferito?");
 
-document.getElementById("password").innerHTML = name+surname+prefColor;
+document.getElementById("password").innerHTML = name+surname+prefColor+"19";
