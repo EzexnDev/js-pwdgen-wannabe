91ab8f51136654b09527d9af70b04142c6b9172b
Gianmarco Cavaliere <gianmarco.cavalieredev@gmail.com>
2020-10-19 15:13:49 +0200

    Esercizio Completo

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var name = prompt("Come ti chiami?");
+
+var surname = prompt("Il tuo cognome?");
+
+var prefColor = prompt("Il tuo colore preferito?");
+
+document.getElementById("password").innerHTML = name+surname+prefColor;
